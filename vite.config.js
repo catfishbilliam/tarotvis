c49647e732f596420b8b3e9e130b5744e9c68e6e
William Conway <wconway@umd.edu>
2024-12-30 22:23:08 -0500

    Fix index.html location for Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,26 +1,12 @@
 import { defineConfig } from 'vite';
-import { resolve } from 'path';
 
 export default defineConfig({
-  root: 'public', // Set 'public' as the root directory
   server: {
     port: 3003,
-    proxy: {
-      '/generate-tarot-story': {
-        target: 'http://localhost:3003',
-        changeOrigin: true,
-        secure: false,
-      },
-    },
   },
   build: {
-    outDir: '../dist',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'public/index.html'),
-    },
-    assetsDir: '', // Keeps assets in root
-    copyPublicDir: true // Ensure public directory is copied
+    outDir: 'dist', // Output directory
+    emptyOutDir: true, // Clear dist folder before build
   },
-  
+  publicDir: 'public', // Copy everything from public/
 });
